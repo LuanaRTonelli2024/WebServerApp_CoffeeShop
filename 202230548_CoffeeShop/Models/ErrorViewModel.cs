37fce0672b8aec6910abba052107b594aca55f4c
Paladin <Paladin@WS-01>
2025-12-02 16:32:25 -0500

    Add project files.

---
+++ 202230548_CoffeeShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _202230548_CoffeeShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
